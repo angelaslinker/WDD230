02b3d614be156f5a23636802eb3e703ec9ef26b2
angelaslinker <angela.r.slinker@gmail.com>
2022-10-28 16:14:58 -0700

    Added discover page

---
+++ discover/js/date.js
@@ -0,0 +1,5 @@
+let date = new Date();
+
+document.getElementById("currentYear").innerHTML = date.getFullYear()
+
+document.getElementById("lastUpdated").innerHTML = document.lastModified
---
+++ discover/js/lazyload.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('img');
+const options = { threshold: 0.5, rootMargin: '0px 0px -100px 0px' };
+
+const preloadImage = (img) => {
+  const src = img.getAttribute('data-src');
+  if (!src) {
+    return;
+  }
+  img.src = src;
+}
+
+const io = new IntersectionObserver((entries, io) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            io.unobserve(entry.target);
+        }
+    });
+}, options);
+
+images.forEach(image => {
+    io.observe(image);
+});
